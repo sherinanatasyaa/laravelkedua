e1090783b45dfa3ff33f124c482705a25b1b6fa5
sherinanatasyaa <shernatasya01@gmail.com>
2021-10-12 01:07:44 +0700

    commit 1

---
+++ resources/views/urutan.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'urutan')
+
+@section('content')
+
+@foreach($numbers as $number)
+
+    <h1>Urutan ke - {{ $number['ke'] }}</h1>
+    <h3>Nomor ke - {{ $number['nomor'] }}</h3>
+
+@endforeach
+@endsection
